de4aaa3eff03ed3b9b7aff21cfd688062354cb28
Alex Seeley <alexseeley134@gmail.com>
2024-08-12 15:57:35 -0600

    Add chakra and title bar mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col items-center p-12">
+        <div className="mt-12 w-full">
+          <div className="flex justify-between items-center flex-nowrap truncate">
+            <p>13</p>
+            <p>Wallet&nbsp;Co.</p>
+          </div>
+          <hr className="w-full border-black " />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="font-bold">testing heavy</p>
-      </div>
-      <p className="font-thin">Testing Light</p>
     </>
   );
 }
