41ca3a9b2c2d6ec2975aae12bb56ee7576d84752
Alex Seeley <alexseeley134@gmail.com>
2024-08-12 21:34:42 -0600

    Add fun feature where number increases as scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import NavButton from "./components/NavButton";
+import { useState } from "react";
 
 function App() {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  document.addEventListener("scroll", () => {
+    setPageNumber(Math.floor(window.scrollY / 60));
+  });
+
   return (
     <>
-      <div className="flex flex-col items-center p-12">
-        <div className="mt-12 w-full">
+      <div className="flex flex-col items-center p-12 sticky top-0 bg-lightOne">
+        <div className="my-12 w-full ">
           <div className="flex justify-between items-center flex-nowrap truncate">
-            <p>13</p>
+            <p>{pageNumber}</p>
             <NavButton />
           </div>
           <hr className="w-full border-black " />
