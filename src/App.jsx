de9b877a3df12a4cdb6c44bae84bc62647a968ee
Alex Seeley <alexseeley134@gmail.com>
2024-08-28 21:04:26 -0600

    Test variable insertion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Nav from "./components/Nav";
 
+let count = 14;
+
 function App() {
   return (
     <>
@@ -14,7 +16,7 @@ function App() {
             className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021
+              Noteworthy technology acquisitions 2021 {count}
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
               Here are the biggest enterprise technology acquisitions of 2021 so
